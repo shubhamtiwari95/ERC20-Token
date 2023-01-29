c1ef9e0da30c858cf0ac5703c000dc2673f52351
Shubham Tiwari <sshubhamtiwari95@gmail.com>
2023-01-29 17:17:11 +0530

    first commit

---
+++ my-token/scripts/deploy.js
@@ -0,0 +1,17 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  const MyToken = await hre.ethers.getContractFactory("MyToken");
+  const myToken = await MyToken.deploy(100000000, 50);
+
+  await myToken.deployed();
+
+  console.log("My Token deployed: ", myToken.address);
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
